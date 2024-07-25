96afac1dc78f79f10381f59bdbc3954e85f4a521
Mauro <maurolmorales@hotmail.com>
2024-07-25 18:58:40 -0300

    ejercicio completo

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const express = require("express");
-
-const app = express();
-const PORT = 3000;
-
-const productsRouter = require("./src/routes/products.router.js");
-const cartsRouter = require("./src/routes/carts.router.js");
-
-app.use(express.json());
-
-app.use("/products", productsRouter);
-app.use("/carts", cartsRouter);
+const app = require("./src/app.js");
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando el http://localhost:${PORT}`);
